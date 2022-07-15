264ca2f7c373cec557e029b52541e612b48f0c70
Mason Gil <mgil@businessolver.com>
2022-07-15 14:59:48 -0500

    current working changes

--- src/server/sofiaTeamsBotBot/dialogBot.ts
+++ src/server/sofiaTeamsBotBot/dialogBot.ts
@@ -4,6 +4,7 @@ import {
     TeamsActivityHandler,
     TurnContext
 } from "botbuilder";
+import { WaterfallStepContext } from "botbuilder-dialogs";
 import { MainDialog } from "./dialogs/mainDialog";
 
 export class DialogBot extends TeamsActivityHandler {
@@ -18,7 +19,32 @@ export class DialogBot extends TeamsActivityHandler {
 
         this.onMessage(async (context, next) => {
             // Run the MainDialog with the new message Activity.
-            await this.dialog.run(context, this.dialogState);
+            // await this.dialog.run(context, this.dialogState);
+            // await next();
+
+            const url = "http://localhost:8080/benefits/livekinnect/admin/processIntentOptionGenesysCloud";
+            const query = {
+                utterance: context.activity.text,
+                mm_num: "310193917",
+                language: "en",
+                sessionId: "0",
+                sessionVars: "null"
+            };
+
+            console.log("test");
+
+            let response = "";
+
+            await fetch(url, {
+                method: "POST",
+                headers: { Authorization: "Basic Z2VuZXN5c2FkbWluLUkzOiFnZW5lc3lzIzI4" },
+                body: JSON.stringify(query)
+            }).then(resp => resp.json()).then(data => {
+                console.log("got a response!");
+                console.log(data);
+                response = data.message;
+            });
+            await context.sendActivity(response);
             await next();
         });
     }
